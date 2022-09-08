6eadd2180a7ddcafc4eba2bbe68cd884e06f42a8
James Robey <james.robey@gmail.com>
2022-09-08 08:19:15 -0500

    Fetch from weather api, populate view with daily and hourly

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,5 +1,13 @@
 class WeatherController < ApplicationController
 
   def index
+    url =  "https://floral-paper-1590.fly.dev/?latitude=40.75&longitude=-74&transform=true"
+    resp = HTTParty.get(url)
+    data = JSON.parse(resp.body)
+
+    @daily = data["daily"]
+    hourly_2x = data["daily"][Date.today.to_s]["hourly"].to_a + data["daily"][Date.tomorrow.to_s]["hourly"].to_a
+    hour = Time.now.hour
+    @hourly = 8.times.map {|i| hourly_2x[hour+3*i] }
   end
 end
